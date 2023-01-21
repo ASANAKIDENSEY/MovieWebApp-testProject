9779455867f8f348ceed6f5bf25de53d9648ff45
ASANAKIDENSEY <asanakidensey@gmail.com>
2023-01-21 12:37:37 +0300

    filmScript

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-let a = 5,
-    b = 5;
-// console.log(a++);
-console.log(7%3);
+'use strict'
+const numberOfFilms = +prompt('Сколько вы посмотрели фильмов?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    private: true,
+};
+
+const a = prompt('Один из последних фильмов которые вы посмотрели?',''),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних фильмов которые вы посмотрели?',''),
+      d = prompt('На сколько оцените его?');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
